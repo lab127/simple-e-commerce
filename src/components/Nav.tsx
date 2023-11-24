9e87af50c54583fe516835c8319ac403c24f7768
unknown <12640394+lab127@users.noreply.github.com>
2023-11-24 13:23:05 +0700

    dark mode menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
   };
   return (
     <>
-      <nav className="bg-gray-100 text-neutral-500 dark:text-neutral-300 dark:bg-black dark:border-b-[1px] dark:border-gray-800">
+      <nav className="bg-gray-100 text-neutral-700 dark:text-neutral-300 dark:bg-black dark:border-b-[1px] dark:border-gray-800">
         <div className="max-w-7xl mx-auto px-7 py-4 flex justify-between">
           <Image
             src="/assets/logo.png"
@@ -53,7 +53,7 @@ export default function Navbar() {
         <div
           className={`${
             isHidden ? "hidden" : "flex flex-col"
-          } md:flex absolute w-full md:w-auto h-screen md:h-auto bg-gray-200 md:bg-gray-100 md:mt-10 md:mx-2 top-0`}
+          } md:flex absolute w-full md:w-auto h-screen md:h-auto bg-gray-100 text-neutral-700 dark:text-neutral-300 dark:bg-black md:mt-10 md:mx-2 top-0`}
         >
           <button
             className="md:hidden flex justify-end px-7 py-11"
@@ -70,7 +70,10 @@ export default function Navbar() {
                 </Link>
               </li>
             ))}
-            <button className="md:px-2 px-4 py-1" type="submit">
+            <button
+              className={`${!isHidden && "hidden"} md:px-2 px-4 py-1`}
+              type="submit"
+            >
               <TbShoppingCart size={30} />
             </button>
           </ul>
