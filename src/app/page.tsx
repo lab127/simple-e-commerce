49785a4c78a633c32f3fd3e872c51b36289a6dbb
unknown <12640394+lab127@users.noreply.github.com>
2023-11-24 13:43:47 +0700

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Nav from "@/components/Nav";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <>
-      <Nav />
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import Navbar from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+}
